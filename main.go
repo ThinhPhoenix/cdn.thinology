eebfcccb5c50cc6261c94cfd3dd92d29a003ba3a
ThinhPhoenix <laithinh1310@gmail.com>
2024-06-21 18:54:07 +0700

    json more field

--- main.go
+++ main.go
@@ -25,26 +25,29 @@ type Response struct {
 }
 
 type FileData struct {
-	FileID    string `json:"file_id"`
-	FileURL   string `json:"file_url"`
-	SecureURL string `json:"secure_url"`
-	FileSize  int    `json:"file_size"`
+	ID        string `json:"id"`
+	URL       string `json:"url"`
+	SecureURL string `json:"secure_url,omitempty"`
+	Bytes     int    `json:"bytes"`
+	Format    string `json:"format"`
 }
 
 type UrlData struct {
-	FileURL   string `json:"file_url"`
+	URL       string `json:"url"`
 	SecureURL string `json:"secure_url"`
 }
 
 var (
-	secureURLs = make(map[string]string)
-	mu         sync.Mutex
+	secureURLs          = make(map[string]string)
+	secureURLExtensions = make(map[string]string)
+	mu                  sync.Mutex
 )
 
-func generateSecureURL(fileURL string) string {
+func generateSecureURL(fileURL, originalExtension string) string {
 	id := uuid.New().String()
 	mu.Lock()
 	secureURLs[id] = fileURL
+	secureURLExtensions[id] = originalExtension
 	mu.Unlock()
 	return id
 }
@@ -59,10 +62,27 @@ func isURLFile(url string) (bool, string, int64, error) {
 	contentType := resp.Header.Get("Content-Type")
 	contentLength := resp.ContentLength
 
-	// Check if Content-Type indicates a file (you can customize this based on your needs)
 	isFile := strings.HasPrefix(contentType, "image/") || strings.HasPrefix(contentType, "application/") || strings.HasPrefix(contentType, "video/") || strings.HasPrefix(contentType, "audio/")
 
 	return isFile, contentType, contentLength, nil
+}
+
+func getExtensionFromContentType(contentType string) string {
+	switch contentType {
+	case "application/zip":
+		return "zip"
+	case "application/x-7z-compressed":
+		return "7z"
+	case "application/pdf":
+		return "pdf"
+	case "image/jpeg":
+		return "jpg"
+	case "image/png":
+		return "png"
+	// Add more mappings as needed
+	default:
+		return ""
+	}
 }
 
 func getActualURL(secureID string) (string, bool) {
@@ -96,10 +116,10 @@ func main() {
 		var fileURL string
 		var fileSize int
 		var err error
+		var fileExt string
 
 		fileHeader, err := c.FormFile("document")
 		if err == nil {
-			// File is uploaded
 			file, err := fileHeader.Open()
 			if err != nil {
 				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to open file"})
@@ -118,12 +138,15 @@ func main() {
 				c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Failed to get file info: %v", err)})
 				return
 			}
-		} else {
-			// Treat document as text input (file URL)
-			fileURL = c.PostForm("document")
-			fileSize = 0 // Assuming fileSize is not relevant for text inputs
 
-			// Check if the file URL points to a valid file
+			fileExt = filepath.Ext(fileHeader.Filename)
+			if fileExt != "" {
+				fileExt = fileExt[1:] // Remove the leading dot
+			}
+		} else {
+			fileURL = c.PostForm("document")
+			fileSize = 0
+
 			isFile, contentType, contentLength, err := isURLFile(fileURL)
 			if err != nil {
 				c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Failed to check URL: %v", err)})
@@ -135,49 +158,44 @@ func main() {
 				return
 			}
 
-			// Optionally, you can use the content type and length if needed
 			fmt.Printf("Content Type: %s, Content Length: %d\n", contentType, contentLength)
+
+			fileExt = filepath.Ext(fileURL)
+			if fileExt != "" {
+				fileExt = fileExt[1:] // Remove the leading dot
+			}
 		}
 
-		// Generate secure URL
 		scheme := c.Request.Header.Get("X-Forwarded-Proto")
 		if scheme == "" {
 			scheme = "http"
 		}
 
-		secureID := generateSecureURL(fileURL)
+		secureID := generateSecureURL(fileURL, fileExt)
 		secureURL := fmt.Sprintf("%s://%s/drive/%s", scheme, c.Request.Host, secureID)
 
 		fileData := FileData{
-			FileID:    fileID,
-			FileURL:   fileURL,
+			ID:     fileID,
+			URL:    fileURL,
 			SecureURL: secureURL,
-			FileSize:  fileSize,
+			Bytes:  fileSize,
+			Format: fileExt,
 		}
 
 		response := Response{
 			Success: true,
-			Message: "File uploaded successfully",
+			Message: "Upload file successfully!",
 			Data:    fileData,
 		}
 
-		jsonResponse, err := json.Marshal(response)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to marshal JSON response"})
-			return
-		}
-
-		c.Header("Content-Type", "application/json")
-		c.Status(http.StatusOK)
-		c.Writer.Write(jsonResponse)
+		c.JSON(http.StatusOK, response)
 	})
 
 	r.GET("/url", func(c *gin.Context) {
 		botToken := c.Query("bot_token")
 		fileID := c.Query("file_id")
 
-		fileURL, fileSize, err := getFileInfo(botToken, fileID)
-		_ = fileSize
+		fileURL, _, err := getFileInfo(botToken, fileID)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Failed to get file info: %v", err)})
 			return
@@ -188,29 +206,26 @@ func main() {
 			scheme = "http"
 		}
 
-		secureID := generateSecureURL(fileURL)
+		fileExt := filepath.Ext(fileURL)
+		if fileExt != "" {
+			fileExt = fileExt[1:] // Remove the leading dot
+		}
+
+		secureID := generateSecureURL(fileURL, fileExt)
 		secureURL := fmt.Sprintf("%s://%s/drive/%s", scheme, c.Request.Host, secureID)
 
 		urlData := UrlData{
-			FileURL:   fileURL,
+			URL:       fileURL,
 			SecureURL: secureURL,
 		}
 
 		response := Response{
 			Success: true,
-			Message: "File URL retrieved successfully",
+			Message: "File URL retrieved successfully!",
 			Data:    urlData,
 		}
 
-		jsonResponse, err := json.Marshal(response)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to marshal JSON response"})
-			return
-		}
-
-		c.Header("Content-Type", "application/json")
-		c.Status(http.StatusOK)
-		c.Writer.Write(jsonResponse)
+		c.JSON(http.StatusOK, response)
 	})
 
 	r.GET("/drive/:id", func(c *gin.Context) {
@@ -234,8 +249,18 @@ func main() {
 			contentType = "application/octet-stream"
 		}
 
-		// Generate a secure or user-friendly filename
-		filename := secureID + ".png" // Example: Replace with your logic to generate a filename
+		mu.Lock()
+		originalExtension := secureURLExtensions[secureID]
+		mu.Unlock()
+
+		if originalExtension == "" {
+			originalExtension = getExtensionFromContentType(contentType)
+		}
+
+		filename := secureID
+		if originalExtension != "" {
+			filename += "." + originalExtension
+		}
 
 		c.Header("Content-Type", contentType)
 		c.Header("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", filename))
@@ -261,19 +286,25 @@ func main() {
 			scheme = "http"
 		}
 
-		secureID := generateSecureURL(fileURL)
+		fileExt := filepath.Ext(fileURL)
+		if fileExt != "" {
+			fileExt = fileExt[1:] // Remove the leading dot
+		}
+
+		secureID := generateSecureURL(fileURL, fileExt)
 		secureURL := fmt.Sprintf("%s://%s/drive/%s", scheme, c.Request.Host, secureID)
 
 		fileData := FileData{
-			FileID:    fileID,
-			FileURL:   fileURL,
+			ID:        fileID,
+			URL:       fileURL,
 			SecureURL: secureURL,
-			FileSize:  fileSize,
+			Bytes:     fileSize,
+			Format:    fileExt,
 		}
 
 		response := Response{
 			Success: true,
-			Message: "Get file information successfully",
+			Message: "Get file information successfully!",
 			Data:    fileData,
 		}
 
@@ -286,13 +317,8 @@ func main() {
 func sendDocument(botToken, chatID string, file io.Reader, fileName string) (string, error) {
 	url := fmt.Sprintf("https://api.telegram.org/bot%s/sendDocument", botToken)
 
-	// Generate a secure ID for the filename
 	secureID := uuid.New().String()
-
-	// Extract file extension from original fileName
 	fileExt := filepath.Ext(fileName)
-
-	// Rename the file to secureID.file_extension
 	newFileName := secureID + fileExt
 
 	body := &bytes.Buffer{}
